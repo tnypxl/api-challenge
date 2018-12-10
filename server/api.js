3d368d500aa9b0b4af4be0735cfcb1cf041b6847
mglaze <michaela.glaze@flexdrive.com>
2018-12-10 15:28:57 -0500

    src files

---
+++ server/api.js
@@ -0,0 +1,11 @@
+const api = module.exports = require('express').Router()
+const products = require('./products');
+const reviews = require('./reviews');
+// import products from './products';
+api
+  .get('/express-test', (req, res) => res.send({express: 'working!'})) //demo route to prove api is working
+  .use('/products', products)
+  .use('/reviews', reviews)
+// No routes matched? 404.
+api.use((req, res) => res.status(404).end())
+api.use((err, req, res) => res.status(200).send(err).end())
