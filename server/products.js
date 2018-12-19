cdfe0c0f1497f0badacb0aa5dd4980bf7f5fef95
tnypxl <github@tnypxl.com>
2018-12-18 23:01:36 -0600

    Fix products endpoint (#1)
    * Fixes products endpoint
    * Fixes issues in app.js
    * Adds test framework

--- server/products.js
+++ server/products.js
@@ -1,31 +1,30 @@
-const db = require('../db') //this is required
+const db = require('../db'); //this is required
 const Product = require('../db/models/product');
 const Review = require('../db/models/review');
-
-const router = require('express').Router()
+const router = require('express').Router();
 
 router.get('/', function(req, res, next) {
-    Product.findAll({
-            include: [Review]
-        })
-        .then(result => {
-            res.status(500).send(result);
-        })
-        .catch(next);
+  Product.findAll()
+    .then(result => res.status(200).send(result))
+    .catch(error => res.status(404).send(error));
 });
 
 router.get('/:id', function(req, res, next) {
-    Product.findOne({
-            where:{id:req.params.id},
-            include: [Review]
-        })
-        .then(result => {
-            if (!result) {
-                var errRes = new Error('Record not found!');
-                return next(errRes)
-            }
-            res.status(200).send(result);
-        })
+  Product.findOne({
+    where:{id:req.params.id},
+    include: [Review]
+  })
+    .then(result => res.status(200).send(result))
+    .catch(error => res.status(404).send(error));
 });
 
-module.exports = router
+router.get('/:id/reviews', function(req, res, next) {
+  Product.findOne({
+    where:{id:req.params.id},
+    include: [Review]
+  })
+    .then(result => res.status(200).send(result.reviews))
+    .catch(error => res.status(404).send(error));
+});
+
+module.exports = router;
