c9a75dbe9dee0001f4805c86f524e244910055f2
Arik Jones <tnypxl00@icloud.com>
2018-12-19 00:39:37 -0600

    Eager-load product and user objects on review endpoints

--- server/reviews.js
+++ server/reviews.js
@@ -1,12 +1,13 @@
 const db = require('../db') //this is required
 const Review = require('../db/models/review');
 const Product = require('../db/models/product');
+const User = require('../db/models/user');
 
 const router = require('express').Router()
 
 router.get('/', function(req, res, next) {
   Review.findAll({
-    include: [Product]
+    include: [Product, User]
   })
     .then(result => res.status(200).send(result))
     .catch(error => res.status(404).send(error));
@@ -15,7 +16,7 @@ router.get('/', function(req, res, next) {
 router.get('/:id', function(req, res, next) {
   Review.findOne({
     where:{id:req.params.id},
-    include: [Product]
+    include: [Product, User]
   })
     .then(result => res.status(200).send(result))
     .catch(error => res.status(404).send(error));
